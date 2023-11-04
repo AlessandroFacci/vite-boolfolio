82e7d23bdb2386e56366817d3d8ecd80fa3e4d81
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-11-04 15:39:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ProjectLits from "./components/projects/ProjectList.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+    ProjectLits,
+  },
+};
+</script>
+
+<template>
+  <ProjectLits />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,28 @@
+<script>
+import ProjectCard from "./ProjectCard.vue";
+
+export default {
+  data() {
+    return {
+      title: "Project List",
+    };
+  },
+
+  components: {
+    ProjectCard,
+  },
+};
+</script>
+
+<template>
+  <div class="container mt-5">
+    <h1>{{ title }}</h1>
+    <div class="row">
+      <div class="col-3">
+        <ProjectCard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
